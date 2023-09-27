85e583535d3905d63e977e8eba7b910efd562de5
Andrés Molina <128313106+IAndresPH@users.noreply.github.com>
2023-09-26 23:12:50 -0500

    Subida de proyecto.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { HeaderComponent } from './header/header.component';
+import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
+import { GaleriaComponent } from './galeria/galeria.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServiciosComponent,
+    HeaderComponent,
+    QuienesSomosComponent,
+    GaleriaComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
