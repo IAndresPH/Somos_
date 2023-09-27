822ca52aebc69b1c558b0a67ed5aad8573a976cb
Sebas-Quiroga <125420726+Sebas-Quiroga@users.noreply.github.com>
2023-09-27 01:27:44 -0500

    implementación de algunos métodos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiciosComponent } from './servicios/servicios.component';
 import { HeaderComponent } from './header/header.component';
 import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
 import { GaleriaComponent } from './galeria/galeria.component';
+import { RouterModule, Routes } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 
+const routes: Routes=[
+  {path: '', redirectTo: './QuienesSomos', pathMatch:'full'},
+  {path: 'Galeria', component:GaleriaComponent},
+  {path: 'QuienesSomos', component:QuienesSomosComponent},
+  {path: 'servicios', component:ServiciosComponent}
+
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,6 +27,7 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule
   ],
   providers: [],
